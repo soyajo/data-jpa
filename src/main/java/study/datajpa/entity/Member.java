a3a73342d12c1ad61633fdcfc1124d95b1272723
josoya <thdi4564@nate.com>
2023-03-16 10:32:16 +0900

    member, team entity 셋팅

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,32 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String userName;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public Member(String userName, int age) {
+        this.userName = userName;
+        this.age = age;
+    }
+}
